f7938bb2edf5a614a3705249f5713652ce706b3f
unbiciclista <primerproyecto@gmail.com>
2015-11-24 21:22:56 +0100

    vamos bien

--- javascripts/custon.js
+++ javascripts/custon.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-            $('#lightgallery').lightGallery();
-        });
+    $('#lightgallery').lightGallery();
+
+  // sticky header
+
+  $(window).scroll(function(){
+  	if ($(this).scrollTop() > 200) { 
+  		$('header').addClass("sticky");
+  	}
+  	else { $('header').removeClass("sticky");
+  }
+  });
+
+  // SMOOTH SCROOL 
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+ });
